18c6ce21551b11f0733ecb86bd2108b943f4d96e
anatoli <anatoli@anatoli.anatoli>
2025-05-19 15:12:21 +0300

    Assets -Rooms:Single Room View Tested & Dusted

--- views/room-details.php
+++ views/room-details.php
@@ -16,7 +16,7 @@ check_login()
 
 
 <body>
-    
+
     <div class="container-scroller">
 
         <!-- partial:partials/_sidebar.html -->
@@ -30,38 +30,163 @@ check_login()
                 <?php
                 //Get room details
                 $room_id = $_GET['room_id'];
-                $stmt = $mysqli->prepare("SELECT * FROM rooms WHERE room_id = ?");
+                $stmt = $mysqli->prepare("SELECT rs.room_title,rs.room_image,rs.room_rent_amount,rs.room_availability,us.user_name AS landlord,user_pic,property_name FROM rooms AS rs 
+                INNER JOIN properties AS ps ON rs.property_id=ps.property_id
+                INNER JOIN users AS us ON ps.property_manager_id=us.user_id
+                 WHERE room_id = ?");
                 $stmt->bind_param('i', $room_id);
                 $stmt->execute();
                 $res = $stmt->get_result();
                 if ($res->num_rows > 0) {
                     $room = $res->fetch_object();
-                } else {
-                  
-                    echo "<script>alert('Room not found'); window.location.href='rooms.php';</script>";
-                }
-                ?>
-                <div class="content-wrapper">
-                    <div class="page-header">
-                        <h3 class="page-title"> Room Details </h3>
-                        <nav aria-label="breadcrumb">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="dashboard">Dashboard</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">Properties</li>
-                            </ol>
-                        </nav>
-                    </div>
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row">
+                    //Declare
+                    if (!empty($room->user_pic)) {
 
-                                <div class="col-12">
+                        $profile_photo = '../public/images/profiles/' . $room->user_pic;
+                    } else {
+
+                        $profile_photo = '../public/images/profiles/dummy_profile.jpg';
+                    }
+                ?>
+                    <div class="content-wrapper">
+                        <div class="page-header">
+                            <h3 class="page-title"><?php echo $room->room_title; ?> Room Details </h3>
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="dashboard">Dashboard</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">View Room details</li>
+                                </ol>
+                            </nav>
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+
+                                    <div class="col-12">
+                                        <!--Show Room details -->
+                                        <div class="col-sm-12 col-md-12 col-xl-8 grid-margin stretch-card">
+                                            <div class="card">
+                                                <div class="card-body">
+                                                    <div class="owl-carousel owl-theme full-width owl-carousel-dash portfolio-carousel owl-loaded owl-drag">
+                                                        <div class="owl-stage-outer">
+                                                            <div class="owl-stage">
+                                                                <div class="owl-item active" style="width: 100%; margin-right: 5px;">
+                                                                    <div class="item">
+                                                                        <img src="../public/images/rooms/<?php echo $room->room_image ?>" class="img-fluid. max-width: 100% " alt="">
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="d-flex py-4">
+                                                        <div class="preview-list w-100">
+                                                            <div class="preview-item p-0">
+                                                                <div class="preview-thumbnail">
+                                                                    <img src="<?php echo $profile_photo ?>" class="rounded-circle" alt="">
+                                                                </div>
+                                                                <div class="preview-item-content d-flex flex-grow">
+                                                                    <div class="flex-grow">
+                                                                        <div class="d-flex d-md-block d-xl-flex justify-content-between">
+                                                                            <h6 class="preview-subject"><?php echo $room->landlord ?></h6>
+                                                                            <p class="text-muted text-small">Landlord</p>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+
+                                                    </div>
+                                                    <div class="col-12">
+                                                        <div class="card">
+                                                            <div class="card-body">
+                                                                <div class="row">
+                                                                    <h2>Basic inform about <?php echo $room->room_title; ?> </h2>
+                                                                    <table class="table table-bordered">
+                                                                        <tr>
+                                                                            <td><b>Room No:</b></td>
+                                                                            <td><?php echo $room->room_title ?></td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td><b>Monthly Rent:</b></td>
+                                                                            <td>Ksh.<?php echo number_format($room->room_rent_amount, 2); ?></td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td><b>Property Name:</b></td>
+                                                                            <td><?php echo $room->property_name; ?></td>
+                                                                        </tr>
+
+                                                                        <tr>
+                                                                            <td> <b>Room status:</b> </td>
+                                                                            <td><?php echo $room->room_availability; ?></td>
+                                                                        </tr>
+
+                                                                    </table>
+                                                                    <h2>Rental History</h2>
+                                                                    <table class="table table-bordered">
+                                                                        <th>
+                                                                        <td>#</td>
+                                                                        <td>From</td>
+                                                                        <td>To</td>
+                                                                        <td>Rented by </td>
+                                                                        <td>Status</td>
+
+                                                                        </th>
+                                                                        <tbody>
+                                                                            <?php
+                                                                            $sql = "SELECT * FROM rental_agreements AS ra 
+                                                                        INNER JOIN users AS us ON ra.tenant_id=us.user_id
+                                                                        WHERE room_id = ?";
+                                                                            $stmt = $mysqli->prepare($sql);
+                                                                            $stmt->bind_param('i', $room_id);
+                                                                            $stmt->execute();
+                                                                            $res = $stmt->get_result();
+                                                                            //Get row count
+                                                                            $row_count = $res->num_rows;
+                                                                            if ($row_count == 0) {
+                                                                                echo "<tr><td colspan='6' class='text-center'>No data found</td></tr>";
+                                                                            } else {
+                                                                                $i = 0;
+                                                                                while ($rental = $res->fetch_object()) {
+                                                                                    $i++;
+                                                                                    echo "<tr>";
+                                                                                    echo "<td>" . $i . "</td>";
+                                                                                    echo "<td>" . date('d M Y', strtotime($rental->rental_start_date)) . "</td>";
+                                                                                    echo "<td>" . date('d M Y', strtotime($rental->rental_end_date)) . "</td>";
+                                                                                    echo "<td>" . $rental->user_name . "</td>";
+                                                                                    echo "<td>" . $rental->agreement_status . "</td>";
+                                                                                    echo "</tr>";
+                                                                                }
+                                                                            }
+                                                                            ?>
+
+
+                                                                        </tbody>
+
+                                                                    </table>
+
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+                                    </div>
+
 
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                <?php
+                } else {
+
+                    echo "<script>alert('Room not found'); window.location.href='rooms.php';</script>";
+                }
+                ?>
+
                 <!-- main-panel ends -->
                 <!-- container-scroller -->
                 <?php include('../functions/custom_alerts.php'); ?>
@@ -71,9 +196,9 @@ check_login()
 
                 <!-- Include Toastr CSS -->
                 <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
-                
+
                 <?php include('../partials/scripts.php') ?>
-                
+
                 <!-- Include Toastr JS -->
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
