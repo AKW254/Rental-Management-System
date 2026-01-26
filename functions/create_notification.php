4efe5a6d268c651639b61a05bbb5b0c0cd694e4e
Antonykilonzo <167535825+AKW254@users.noreply.github.com>
2026-01-26 19:09:32 +0300

    Refactor create_notification function and add image
    Simplified the create_notification function in create_notification.php by removing session and config dependencies, correcting parameter types, and improving formatting. Added a new image file to public/images/rooms.

--- functions/create_notification.php
+++ functions/create_notification.php
@@ -1,19 +1,14 @@
 <?php
-session_start();
-include('../config/config.php');
-header('Content-Type: application/json');
-include('../config/checklogin.php');
-check_login();
-$response = ['success' => false];
-// Declare variables
-$recipient_id = $_SESSION['user_id'];
 
-function create_notification($mysqli, $recipient_id, $title, $message,$status)  {
-    $stmt = $mysqli->prepare("INSERT INTO notifications (user_id,notification_type,notification_message,sent_at,notification_status) VALUES (?, ?, ?, NOW(), ?)");
-    $stmt->bind_param('issis', $recipient_id, $title, $message,$status);
-    if ($stmt->execute()) {
-        return true;
-    } else {
-        return false;
-    }
+function create_notification($mysqli, $recipient_id, $title, $message, $status)
+{
+    $stmt = $mysqli->prepare(
+        "INSERT INTO notifications 
+        (user_id, notification_type, notification_message, sent_at, notification_status) 
+        VALUES (?, ?, ?, NOW(), ?)"
+    );
+
+    $stmt->bind_param('issi', $recipient_id, $title, $message, $status);
+
+    return $stmt->execute();
 }
